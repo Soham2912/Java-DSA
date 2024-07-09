cec29a6196384ec610b8e06ff5676b4747eadd32
Soham2912 <sohamlahoti@gmail.com>
2024-07-09 22:20:43 +0530

    DSA

---
+++ CountDigits.java
@@ -0,0 +1,21 @@
+// Given a number n. Count the number of digits in n which evenly divide n. Return an integer, total number of digits of n which divides n evenly.
+
+// Note :- Evenly divides means whether n is divisible by a digit i.e. leaves a remainder 0 when divided.
+
+class Solution{
+    static int evenlyDivides(int N){
+        // code here
+        int c=0;
+        String s=String.valueOf(N);
+        char a[]= s.toCharArray();
+        for(int i=0;i<a.length;i++){
+        int m=Character.getNumericValue(a[i]);
+        if(m==0){
+            continue;
+        }
+        if(N%m==0){
+            c++;
+        }    
+        }return c;
+    }
+}
